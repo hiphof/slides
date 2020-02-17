cc97139d6887eea0c7ae3a9336d71a77f83a1887
Thijs <35424969+hiphof@users.noreply.github.com>
2020-02-17 11:47:06 +0100

    deleted unnecessary console.log

--- src/index.js
+++ src/index.js
@@ -14,41 +14,30 @@ You! &#9992;
   <br><button  id="inputbutton">Make some slides!</button>
   </div>
   </form>
-  <div id="hier"></div>
 `;
-// onclick="location.href='slides.html';"
+
 document.querySelector("#inputbutton").addEventListener("click", pushbutton);
 
 function pushbutton() {
   let text_array = "";
-  //console.log("dirk");
+
   let inputvalue = document.querySelector("#inputarea").value;
-  console.log(inputvalue);
-  //document.querySelector("#hier").innerHTML =
-  //  '<a href="slides.html">' + inputvalue + "</a>";
+
   text_array = inputvalue.split(/\r?\n/);
-  console.log(text_array);
+
   document.querySelector("#app").innerHTML = "";
   document.querySelector("#app").style.display = "none";
 
-  console.log("piet" + text_array[1]);
-  //return text_array;
-
-  console.log("oke" + text_array[1]);
   document.querySelector("#slides").style.display = "flex";
-  //hoi
-  //`;
+
   let step = 0;
   let somewhere = document.querySelector("#slides");
   somewhere.addEventListener("click", myfunction);
 
   function myfunction() {
-    console.log("hoi" + text_array[1]);
     document.querySelector("#slides").innerHTML = text_array[step];
     document.body.style.backgroundColor = getRandomColor();
     step += 1;
-
-    console.log(step);
   }
   function getRandomColor() {
     var letters = "0123456789ABCDEF";
