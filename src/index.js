4b3b7acf2af2d807ef9ed3a7aff4e9f03cb6da09
Thijs <35424969+hiphof@users.noreply.github.com>
2020-10-08 15:27:22 +0200

    add whatsapp share button on mobile

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import "./styles.css";
+import { Base64 } from "js-base64";
+import * as ClipboardJS from "clipboard";
+
+//new ClipboardJS('.btn');
 
 const initial_content = `&#127856;
 Happy
@@ -25,16 +29,35 @@ function createSlides() {
 }
 
 function pushButton() {
+  document.querySelector("#sendurl").style.display = "flex";
   let textarea_input = document.querySelector("#inputarea").value;
-  //const content_encoded = encodeURI(initial_content);
-  //console.log(content_encoded);
-  //document.querySelector("#sendurl").innerHTML = content_encoded;
-  displaySlides(textarea_input);
+  const content_encoded = Base64.encodeURI(textarea_input);
+  let slidesUrl = window.location.origin + "/?m=" + content_encoded;
+
+  if (
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    )
+  ) {
+    // true for mobile device
+    document.querySelector("#sendurl").innerHTML +=
+      '<a href="whatsapp://send?text=' +
+      slidesUrl +
+      '">Share link via Whatsapp!</a>';
+  } else {
+    // false for not mobile device
+    document.querySelector("#sendurl").innerHTML =
+      '<a href="' + slidesUrl + '">' + slidesUrl + "</a>";
+    //const clipboard = new ClipboardJS();
+    //clipboard.on("success");
+    //document.write("not mobile device");
+  }
 }
 
-function displaySlides(textarea_input) {
+function displaySlides(encoded_message_get) {
   let content_array = "";
-  content_array = textarea_input.split(/\r?\n/);
+  let greeting_string = Base64.decode(encoded_message_get);
+  content_array = greeting_string.split(/\r?\n/);
   let number_of_slides = content_array.length;
   console.log(number_of_slides);
 
